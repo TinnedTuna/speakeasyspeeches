2700828967eb21ed498f246314b78f073d3efaa8
Dan Turner <dan.turner@york.ac.uk>
2014-07-06 09:23:49 +0100

    Added blog model, requirements.txt

--- app/models.py
+++ app/models.py
@@ -4,6 +4,7 @@ import markdown
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(60), unique=True, nullable=True)
+    display_name = db.Column(db.Text)
     password = db.Column(db.Text, nullable=False)
 
     def is_authenticated(self):
@@ -22,6 +23,16 @@ class User(db.Model):
         return 'User:{id: %r, username: %s}' % (self.id, self.username)
 
 class Page(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.Text, nullable=False, unique=True)
+    content = db.Column(db.Text, nullable=False)
+    timestamp = db.Column(db.DateTime, nullable=False)
+    author_user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    def __repr__(self):
+        return 'Post:{id: %r, title: %s}' % (self.id, self.title)
+
+class Blog(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.Text, nullable=False, unique=True)
     content = db.Column(db.Text, nullable=False)
