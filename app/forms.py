3adecd329f057d301bf24860469a9fb8efbbeb83
Dan Turner <dan.turner@york.ac.uk>
2014-07-06 20:36:45 +0100

    Added footer, start of user mgmt. form

--- app/forms.py
+++ app/forms.py
@@ -14,3 +14,9 @@ class BlogPost(Form):
     title = TextField('title', validators = [Required()])
     content = TextAreaField('content', validators= [Required()])
 
+class UserForm(Form):
+    username = TextField('username', validators = [Required()])
+    display_name = TextField('display_name')
+    new_password = TextField('new_password')
+    confirm_password = TextField('confirm_password')
+
