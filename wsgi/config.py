7de9d3f1f3a534f25033a1772837be3a8ed6d686
Dan Turner <dan.turner@york.ac.uk>
2014-07-08 22:57:05 +0100

    Removed spurious db, corrected config

--- wsgi/config.py
+++ wsgi/config.py
@@ -5,8 +5,8 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
 
-if (os.environ['DATABASE_URL']):
-    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+if 'OPENSHIFT_POSTGRESQL_DB_URL' in os.environ:
+    SQLALCHEMY_DATABASE_URI = os.environ['OPENSHIFT_POSTGRESQL_DB_URL']
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir,'db_repository')
 
 CSRF_ENABLED = True
