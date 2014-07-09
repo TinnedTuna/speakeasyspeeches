88df4362d42f058bcd6a26aebb278150295bca9e
Dan Turner <dan.turner@york.ac.uk>
2014-07-09 21:13:56 +0100

    Started the refactor to Blueprints with Pages

--- wsgi/app/__init__.py
+++ wsgi/app/__init__.py
@@ -2,9 +2,11 @@ from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
 from flask.ext.bcrypt import Bcrypt
 from flask.ext.login import LoginManager
+from views import pages
 
 app = Flask(__name__)
 app.config.from_object("config")
+app.register_blueprint(pages)
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 lm = LoginManager()
