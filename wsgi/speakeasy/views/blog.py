2f832b595c1c189a6782a8ee43727b0549f11c2d
Dan Turner <dan.turner@york.ac.uk>
2014-08-02 12:19:01 +0100

    Updating blog posts now works post-refactor

--- wsgi/speakeasy/views/blog.py
+++ wsgi/speakeasy/views/blog.py
@@ -1,8 +1,10 @@
-from flask import Blueprint, render_template, abort
+from flask import Blueprint, render_template, abort, flash
 from flask.ext.login import login_required, current_user
 import speakeasy
-from speakeasy.database import Blog
+from speakeasy.database import Blog, db_session
+from speakeasy.forms import BlogPost
 from speakeasy.views.utils import menu, blog_menu_location
+
 
 import datetime
 
@@ -47,8 +49,8 @@ def edit_post(id):
     if form.validate_on_submit():
         post.title = form.title.data
         post.content = form.content.data
-        db.session.add(post)
-        db.session.commit()
+        db_session.add(post)
+        db_session.commit()
         flash("Post successfully updated")
     else:
         flash("Failed to update post, failed to validate user input.")
@@ -64,4 +66,4 @@ def create_blog_post():
                 author_user_id=current_user.id, \
                 content=form.content.data, \
                 timestamp=datetime.datetime.now())
-        db.session.add(new_post)
+        db_session.add(new_post)
