62076f44cf277109c1909ae47f471d463ea5850e
Dan Turner <dan.turner@york.ac.uk>
2014-08-02 22:33:27 +0100

    Added site config to authentication views

--- wsgi/speakeasy/views/authentication.py
+++ wsgi/speakeasy/views/authentication.py
@@ -3,7 +3,7 @@ from flask.ext.login import login_required, current_user, login_user, logout_use
 import speakeasy
 from speakeasy import bcrypt
 from speakeasy.database import User 
-from speakeasy.views.utils import menu
+from speakeasy.views.utils import menu, site_config
 from speakeasy.forms import LoginForm
 
 import datetime
@@ -16,7 +16,7 @@ auth = Blueprint('auth', __name__,
 @auth.route('/login', methods = ['GET'])
 def login():
     form = LoginForm()
-    return render_template('login.html', title='Login', form=form, menu=menu())
+    return render_template('login.html', title='Login', form=form, menu=menu(), site_config=site_config())
 
 @auth.route('/logout')
 def logout():
@@ -31,15 +31,15 @@ def authenticate():
         user = User.query.filter_by(username=form.username.data).first()
         if user is None:
             flash("Error, incorrect username or password")
-            return render_template('login.html', title='Login', form=form, menu=menu())
+            return render_template('login.html', title='Login', form=form, menu=menu(), site_config=site_config())
         if bcrypt.check_password_hash(user.password, form.password.data):
             login_user(user)
             flash('Login was successful for %s' % (repr(user.display_name)))
             return redirect("/index")
         else:
             flash("Error, incorrect username or password")
-            return render_template('login.html', title='Login', form=form, menu=menu())
+            return render_template('login.html', title='Login', form=form, menu=menu(), site_config=site_config())
     else:
         flash("Error, incorrect username or password")
-        return render_template('login.html', title='Login', form=form, menu=()) 
+        return render_template('login.html', title='Login', form=form, menu=(), site_config=site_config()) 
 
